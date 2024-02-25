605d3d9137b6a36a9a32f77063d3f52eacaa442b
FastWolf-051 <138472083+FastWolf-051@users.noreply.github.com>
2024-02-25 22:54:15 +0300

    removed commentts

--- code/UseHeaders.h
+++ code/UseHeaders.h
@@ -1,23 +1,12 @@
 #if !defined(UseHeaders)
 #define UseHeaders
-// System Libraries
-// Input/Output
 #include <iostream>
-// Additional libraries
-// GLAD
 #include <GLAD/include/glad/glad.h>
-// Block "LNK2019" in GLAD
 #include <GLAD/src/glad.c>
-// GLFW
 #include <GLFW/include/GLFW/glfw3.h>
-// OpenGL math
 #include <GLM/glm/glm.hpp>
-// OpenGL matrix transform
 #include <GLM/glm/gtc/matrix_transform.hpp>
-// Block "LNK2019" in stb_image
 #define STB_IMAGE_IMPLEMENTATION
-// Stb_image
 #include <STB/stb_image.h>
-// For SetConsoleTitleA
 #include <windows.h>
-#endif // UseHeaders
+#endif
