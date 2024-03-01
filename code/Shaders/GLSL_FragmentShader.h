22e156ab3ff7ad388fc61e7e60fe632b464fb8f4
FastWolf-051 <138472083+FastWolf-051@users.noreply.github.com>
2024-03-01 17:10:22 +0300

    Maked a gradient triangle 🙃

--- code/Shaders/GLSL_FragmentShader.h
+++ code/Shaders/GLSL_FragmentShader.h
@@ -5,7 +5,8 @@
 GLSL_SCRIPT fragmentShaderSource =
 GLSL_INIT_VERSION
 GLSL_OUT_VECTOR4 "FragColor;" BR
+GLSL_INPUT_VEC3 "OurColor;" BR
 GLSL_EXECUTE_START
-"   FragColor = vec4(0.3f, 0.9f, 0.4f, 0.3f);" BR
+"   FragColor = vec4(OurColor,1.0f);" BR
 GLSL_EXECUTE_END;
 #endif
--- code/Shaders/GLSL_VertexShader.h
+++ code/Shaders/GLSL_VertexShader.h
@@ -5,7 +5,10 @@
 GLSL_SCRIPT vertexShaderSource =
 GLSL_INIT_VERSION
 GLSL_LAYOUT_0_VEC3 "aPos;" BR
+GLSL_LAYOUT_1_VEC3 "aColor;" BR
+GLSL_OUT_VECTOR3 "OurColor;" BR
 GLSL_EXECUTE_START
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);" BR
+"   gl_Position = vec4(aPos,1.0f);" BR
+"	OurColor = aColor;" BR
 GLSL_EXECUTE_END;
 #endif
